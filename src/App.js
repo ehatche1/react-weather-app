d58b1beb865c3baf0f429ebe9061c7c92377484a
ehatche1 <140868222+ehatche1@users.noreply.github.com>
2024-07-03 21:58:59 -0400

    imported files from test-react-weather-app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/egypthatchett626/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Egypt Hatchett
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/ehatche1/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on Github
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
